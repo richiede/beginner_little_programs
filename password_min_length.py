340e6fa26d0080c65c450559f0ae4deb75b1754e
richiede <68747135+richiede@users.noreply.github.com>
2021-06-24 08:54:53 +0100

    Update password_min_length.py

--- password_min_length.py
+++ password_min_length.py
@@ -1,12 +1,12 @@
 # This program sets a variable called user_password.
-# If the password is less that 5 characters, the program loops round to re-enter the password
+# If the password is less than 5 characters or more than 12, the program loops round to re-enter the password
 
 user_password = input('Set a password - minimum 5 characters please: ')
 
 while True:
-	if len(user_password) < 5:
-		print('Password less that 5 characters... Try again: ')
-		user_password = input('Set a password - minimum 5 characters please: ')
+	if len(user_password) < 5 or len(user_password) > 12:
+		print('Password length invalid ... Try again: ')
+		user_password = input('Set a password - min 5 max 12: ')
 	else:
 		print('Password has been set!')
 		break
