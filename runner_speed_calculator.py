73a279ac0832286f00e64b9087ccd06806b00195
richiede <68747135+richiede@users.noreply.github.com>
2021-07-15 16:25:45 +0100

    Update runner_speed_calculator.py

--- runner_speed_calculator.py
+++ runner_speed_calculator.py
@@ -1,8 +1,16 @@
 # This works out the average speed of a track runner
+# Can't be a negative or 0 time
 
 distance = float(input('What distance did you run in meters? '))
 
 time = float(input('What was your time in seconds? '))
+
+while not time > 0:
+	try:
+		print('Time has to be positive. Try again please')
+		time = float(input('What was your time in seconds? '))
+	except ZeroDivisionError:
+		print('Can\'t be a 0 value....')
 
 speed = round(distance/time, 2)
 
